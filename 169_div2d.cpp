2aee484ed9d95421aeae610a9033050926306bcc
atyamsriharsha <atyamsriharsha@gmail.com>
2015-11-07 18:23:04 +0530

    another one

---
+++ 169_div2d.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main()
+{ 
+	long long l,r ;
+	cin >> l >> r ;
+	if(l==1 && r==1)
+		cout << 0 ;
+	else
+	cout << ((1LL<<(64-__builtin_clzll(l^r)))-1LL) ;
+}
